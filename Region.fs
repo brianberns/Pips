b50ea3ee4a36e1e7854ea3fdc69af0af59a531ef
Brian Berns <brianberns@gmail.com>
2025-10-02 09:51:07 -0400

    Polish.

--- Region.fs
+++ Region.fs
@@ -22,24 +22,19 @@ module Region =
             |> Array.choose (fun cell ->
                 Board.tryGetValue cell board)
 
-    let tryGetValues board region =
+    let isSolved board region =
         let values = getValues board region
         if values.Length = region.Cells.Length then
-            Some values
-        else
-            None
-
-    let isSolved board region =
-        match tryGetValues board region, region.Type with
-            | None, _ -> false
-            | Some _, RegionType.Unconstrained -> true
-            | Some values, RegionType.Equal ->
-                (Array.distinct values).Length = 1
-            | Some values, RegionType.Unequal ->
-                (Array.distinct values).Length = values.Length
-            | Some values, RegionType.SumLess n ->
-                Array.sum values < n
-            | Some values, RegionType.SumGreater n ->
-                Array.sum values > n
-            | Some values, RegionType.Sum n ->
-                Array.sum values = n
+            match region.Type with
+                | RegionType.Unconstrained -> true
+                | RegionType.Equal ->
+                    (Array.distinct values).Length = 1
+                | RegionType.Unequal ->
+                    (Array.distinct values).Length = values.Length
+                | RegionType.SumLess n ->
+                    Array.sum values < n
+                | RegionType.SumGreater n ->
+                    Array.sum values > n
+                | RegionType.Sum n ->
+                    Array.sum values = n
+        else false
