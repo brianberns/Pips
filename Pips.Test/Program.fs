e0597edcdc84cc8699993d988d75b8cd67e9492b
Brian Berns <brianberns@gmail.com>
2025-10-02 17:26:06 -0400

    More work on test case search.

--- Pips.Test/Program.fs
+++ Pips.Test/Program.fs
@@ -7,38 +7,34 @@ open System.Threading
 module Program =
 
     let solve (date : DateOnly) =
-
-        printfn $"{date}"
-
         let puzzleMap =
             let dateStr = date.ToString("yyyy-MM-dd")
             Daily.loadHttp $"https://www.nytimes.com/svc/pips/v1/{dateStr}.json"
-
         let stopwatch = Stopwatch.StartNew()
-        let solutions = Puzzle.solve puzzleMap["hard"]
-        printfn $"Found {solutions.Length} solution(s) in {stopwatch.Elapsed}:"
-        printfn ""
-        printfn $"{Puzzle.printBoard solutions[0]}"
-
+        let _solutions = Puzzle.solve puzzleMap["hard"]
+        stopwatch.Elapsed.TotalSeconds
 
     let run timeout work =
         let work =
             async {
                 let! child =
                     Async.StartChild(
-                        async { return work () },
+                        async { return Some (work ()) },
                         timeout)
                 return! child
             }
         try
             Async.RunSynchronously(work, timeout)
         with :? TimeoutException ->
-            printfn "Timeout"
-            printfn ""
+            None
 
     let startDate = DateOnly.Parse("9/1/2025")
-    for offset = 0 to 30 do
-        let work () =
-            solve (startDate.AddDays(offset))
-        run 10000 work
-    printfn "End"
+    [ 0 .. 30 ]
+        |> Seq.map (fun offset ->
+            let date = startDate.AddDays(offset)
+            let work () = solve date
+            let timeOpt = run 10000 work
+            printfn $"{date}: {timeOpt}"
+            date, timeOpt)
+        |> Seq.maxBy snd
+        |> printfn "%A"
