83212eb50721601a1d2eda04c846c45f0107eff4
Brian Berns <brianberns@gmail.com>
2025-10-02 16:30:50 -0400

    Added unit test.

--- Pips.Test/Tests.fs
+++ Pips.Test/Tests.fs
@@ -1,8 +1,35 @@
-module Tests
+namespace Pips
 
-open System
 open Xunit
 
-[<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+module Test =
+
+    let private place
+        domino
+        (leftRow, leftCol)
+        (rightRow, rightCol)
+        board =
+        Board.place
+            domino
+            { Row = leftRow; Column = leftCol }
+            { Row = rightRow; Column = rightCol }
+            board
+
+    let private puzzleMap = Daily.loadFile "Daily.json"
+
+    [<Fact>]
+    let Easy () =
+        let puzzle = puzzleMap["easy"]
+        let dominoes = Seq.toArray puzzle.UnplacedDominoes
+        let expected =
+            Array.singleton {
+                puzzle with
+                    UnplacedDominoes = List.empty
+                    Board =
+                        puzzle.Board
+                            |> place dominoes[0] (0, 1) (0, 2)
+                            |> place dominoes[1] (2, 2) (2, 1)
+                            |> place dominoes[2] (1, 1) (1, 0)
+                            |> place dominoes[3] (1, 2) (1, 3)
+            }
+        Assert.Equal(expected, Puzzle.solve puzzle)
