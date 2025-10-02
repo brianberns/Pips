756f35de2ee45a45439d887f57d47b675137743f
Brian Berns <brianberns@gmail.com>
2025-10-02 16:42:40 -0400

    Began search for test puzzle.

--- Pips.Test/Program.fs
+++ Pips.Test/Program.fs
@@ -1,18 +1,24 @@
 ﻿namespace Pips
 
+open System
 open System.Diagnostics
 
 module Program =
 
-    let puzzleMap = Daily.loadFile "2025-09-30.json"
+    let solve (date : DateOnly) =
 
-    for name in [ "easy"; "medium"; "hard" ] do
-        printfn ""
-        printfn $"{name}:"
-        printfn ""
+        printfn $"{date}"
+
+        let puzzleMap =
+            let dateStr = date.ToString("yyyy-MM-dd")
+            Daily.loadHttp $"https://www.nytimes.com/svc/pips/v1/{dateStr}.json"
+
         let stopwatch = Stopwatch.StartNew()
-        let solutions = Puzzle.solve puzzleMap[name]
-        printfn $"Found {solutions.Length} solution(s) in {stopwatch.Elapsed}"
+        let solutions = Puzzle.solve puzzleMap["hard"]
+        printfn $"Found {solutions.Length} solution(s) in {stopwatch.Elapsed}:"
         printfn ""
-        for solution in solutions do
-            printfn $"{Puzzle.printBoard solution}"
+        printfn $"{Puzzle.printBoard solutions[0]}"
+
+    let startDate = DateOnly.Parse("9/1/2025")
+    for offset = 0 to 30 do
+        solve (startDate.AddDays(offset))
