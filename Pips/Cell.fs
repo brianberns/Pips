3cc40093adb1d95417ca2b402e7f7bc4b4029389
Brian Berns <brianberns@gmail.com>
2025-10-08 10:07:38 -0400

    Began integration of edge-checking logic.

--- Pips/Cell.fs
+++ Pips/Cell.fs
@@ -9,6 +9,16 @@ type Cell =
 
 module Cell =
 
+    /// Gets all possible cells adjacent to the given cell.
+    /// Some of these cells might not actually exist, though.
+    let getAdjacent cell =
+        [|
+            { cell with Row = cell.Row - 1 }
+            { cell with Row = cell.Row + 1 }
+            { cell with Column = cell.Column - 1 }
+            { cell with Column = cell.Column + 1 }
+        |]
+
     let adjacent cellA cellB =
         (cellA.Row = cellB.Row
             && abs (cellA.Column - cellB.Column) = 1)
