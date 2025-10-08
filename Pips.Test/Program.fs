72ff49742563ce376ef3e6ee4a742d64fe36b9a9
Brian Berns <brianberns@gmail.com>
2025-10-08 11:31:41 -0400

    Added test.

--- Pips.Test/Program.fs
+++ Pips.Test/Program.fs
@@ -13,38 +13,51 @@ module Program =
         let solutions = Puzzle.solve puzzleMap["hard"]
         stopwatch.Elapsed.TotalSeconds, solutions
 
-    let run timeout work =
-        let work =
-            async {
-                let! child =
-                    Async.StartChild(
-                        async { return Some (work ()) },
-                        timeout)
-                return! child
-            }
-        try
-            Async.RunSynchronously(work, timeout)
-        with :? TimeoutException ->
-            None
+    let solveMonth () =
 
-    let print date resultOpt =
-        match resultOpt with
-            | Some (time, (puzzles : List<_>)) ->
-                printfn $"{date}: {time} seconds, {puzzles.Length} solution(s)"
-                printfn ""
-                printfn $"{Puzzle.printBoard puzzles[0]}"
-            | None ->
-                printfn $"{date}: timeout"
+        let run timeout work =
+            let work =
+                async {
+                    let! child =
+                        Async.StartChild(
+                            async { return Some (work ()) },
+                            timeout)
+                    return! child
+                }
+            try
+                Async.RunSynchronously(work, timeout)
+            with :? TimeoutException ->
+                None
 
-    let startDate = DateOnly.Parse("9/1/2025")
-    let date, resultOpt =
-        [ 0 .. 30 ]
-            |> Seq.map (fun offset ->
-                let date = startDate.AddDays(offset)
-                let work () = solve date
-                let resultOpt = run 100000 work
-                print date resultOpt
-                date, resultOpt)
-            |> Seq.maxBy snd
-    printfn "Longest:"
-    print date resultOpt
+        let print date resultOpt =
+            match resultOpt with
+                | Some (time, (puzzles : List<_>)) ->
+                    printfn $"{date}: {time} seconds, {puzzles.Length} solution(s)"
+                    printfn ""
+                    printfn $"{Puzzle.printBoard puzzles[0]}"
+                | None ->
+                    printfn $"{date}: timeout"
+
+        let startDate = DateOnly.Parse("9/1/2025")
+        let date, resultOpt =
+            [ 0 .. 30 ]
+                |> Seq.map (fun offset ->
+                    let date = startDate.AddDays(offset)
+                    let work () = solve date
+                    let resultOpt = run 100000 work
+                    print date resultOpt
+                    date, resultOpt)
+                |> Seq.maxBy snd
+        printfn "Longest:"
+        print date resultOpt
+
+    let solveOne () =
+        let puzzleMap = Daily.loadHttp "https://www.nytimes.com/svc/pips/v1/2025-10-01.json"
+        let stopwatch = Stopwatch.StartNew()
+        let solutions = Puzzle.solve puzzleMap["hard"]
+        printfn $"Found {solutions.Length} solution(s) in {stopwatch.Elapsed}"
+        printfn ""
+        for solution in solutions do
+            printfn $"{Puzzle.printBoard solution}"
+
+    solveOne ()
