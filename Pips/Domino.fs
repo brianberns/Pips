a3949166460501f53287db278bbdaacf56806955
Brian Berns <brianberns@gmail.com>
2025-10-08 23:33:41 -0400

    Commented.

--- Pips/Domino.fs
+++ Pips/Domino.fs
@@ -1,14 +1,30 @@
 ﻿namespace Pips
 
+(*
+ * ┌───────┬───────┐
+ * │ o   o │       │
+ * │ o   o │   o   │
+ * │ o   o │       │
+ * └───────┴───────┘
+ *)
+
+/// Number of pips on one side of a domino.
 type PipCount = int
 
 module PipCount =
 
+    /// Smallest pip count.
     let minValue = 0
+
+    /// Largest pip count.
     let maxValue = 6
 
+/// Left and right pips.
 type Domino =
     {
+        /// Left pips.
         Left : PipCount
+
+        /// Right pips.
         Right : PipCount
     }
