e56f8ff19d883707caa749f92528fa6742f05981
Brian Berns <brianberns@gmail.com>
2025-10-09 15:25:12 -0400

    Refactored validation functions.

--- Pips/Region.fs
+++ Pips/Region.fs
@@ -53,35 +53,71 @@ module Region =
             let elem = array[0]
             Array.forall ((=) elem) array[1..]   // fail early if a second value is found
 
+    /// Validates an Equal region.
+    let private validateEqual board region =
+        assert(region.Type.IsEqual)
+        let pipCounts = getPipCounts board region
+        hasLessThanTwoDistinct pipCounts
+
+    /// Validates an Unequal region.
+    let private validateUnequal board region =
+        assert(region.Type.IsUnequal)
+        let pipCounts = getPipCounts board region
+        (Array.distinct pipCounts).Length = pipCounts.Length
+
+    /// Validates a SumLess region.
+    let private validateSumLess n board region =
+        assert(region.Type.IsSumLess)
+        let pipCounts = getPipCounts board region
+        assert(PipCount.minValue = 0)
+        Array.sum pipCounts < n
+
+    /// Validates a SumGreater region.
+    let private validateSumGreater n board region =
+        assert(region.Type.IsSumGreater)
+        let pipCounts = getPipCounts board region
+
+            // are there still enough uncovered cells to exceed the target?
+        let nEmpty = region.Cells.Length - pipCounts.Length
+        (Array.sum pipCounts) + (PipCount.maxValue * nEmpty) > n
+
+
+    /// Validates a Sum region.
+    let private validateSum n board region =
+        assert(region.Type.IsSum)
+        let pipCounts = getPipCounts board region
+        let sum = Array.sum pipCounts
+
+            // all cells covered?
+        if pipCounts.Length = region.Cells.Length then
+            sum = n
+        else
+            assert(PipCount.minValue = 0)
+
+                // are there still enough uncovered cells to reach the target?
+            if sum <= n then
+                let nEmpty = region.Cells.Length - pipCounts.Length
+                sum + (PipCount.maxValue * nEmpty) >= n
+            else false
+
+    /// Validates the given region on the given board. The region
+    /// may still have uncovered cells.
     let isValid board region =
         match region.Type with
             | RegionType.Any -> true
             | RegionType.Equal ->
-                let pipCounts = getPipCounts board region
-                hasLessThanTwoDistinct pipCounts
+                validateEqual board region
             | RegionType.Unequal ->
-                let pipCounts = getPipCounts board region
-                (Array.distinct pipCounts).Length = pipCounts.Length
+                validateUnequal board region
             | RegionType.SumLess n ->
-                let pipCounts = getPipCounts board region
-                assert(PipCount.minValue = 0)
-                Array.sum pipCounts < n
+                validateSumLess n board region
             | RegionType.SumGreater n ->
-                let pipCounts = getPipCounts board region
-                let nEmpty = region.Cells.Length - pipCounts.Length
-                (Array.sum pipCounts) + (PipCount.maxValue * nEmpty) > n
+                validateSumGreater n board region
             | RegionType.Sum n ->
-                let pipCounts = getPipCounts board region
-                let sum = Array.sum pipCounts
-                if pipCounts.Length = region.Cells.Length then
-                    sum = n
-                else
-                    assert(PipCount.minValue = 0)
-                    if sum <= n then
-                        let nEmpty = region.Cells.Length - pipCounts.Length
-                        sum + (PipCount.maxValue * nEmpty) >= n
-                    else false
+                validateSum n board region
 
+    /// Determines whether the given region on the given board has
+    /// been solved (with no uncovered cells).
     let isSolved board region =
         let pipCounts = getPipCounts board region
         if pipCounts.Length = region.Cells.Length then
