9950b0d14187d6dd3aefb5d3bca25cb2c03018be
Brian Berns <brianberns@gmail.com>
2025-10-09 00:07:48 -0400

    Draw domino outlines.

--- Pips.Test/Program.fs
+++ Pips.Test/Program.fs
@@ -14,23 +14,84 @@ module Program =
         stopwatch.Elapsed.TotalSeconds, solutions
 
     let printBoard puzzle =
+
         let maxRow =
             puzzle.Regions
                 |> Array.collect _.Cells
                 |> Array.map _.Row
                 |> Array.max
+
         let maxCol =
             puzzle.Regions
                 |> Array.collect _.Cells
                 |> Array.map _.Column
                 |> Array.max
+
+        let dominoMap =
+            puzzle.Board.Dominoes
+                |> Seq.collect (fun (_, c1, c2) ->
+                    let d = min c1 c2, max c1 c2
+                    [ c1, d; c2, d ])
+                |> Map
+
+        let inSameDomino c1 c2 =
+            Map.tryFind c1 dominoMap = Map.tryFind c2 dominoMap
+        
+        let isCellEmpty cell =
+            if cell.Row < 0
+                || cell.Row > maxRow
+                || cell.Column < 0
+                || cell.Column > maxCol then true
+            else
+                puzzle.Board[cell] = Board.empty
+
         for row in 0 .. maxRow do
+
+                // print top border
             for col in 0 .. maxCol do
-                let cell = { Row = row; Column = col }
+                let cell = Cell.create row col
+                let topCell = { cell with Row = row - 1 }
+                if (not (isCellEmpty cell) || not (isCellEmpty topCell))
+                    && not (inSameDomino cell topCell) then
+                    printf "+---"
+                else
+                    printf "+   "
+            printfn "+"
+
+                // print cell content and side borders
+            for col in 0 .. maxCol do
+
+                let cell = Cell.create row col
+
+                let leftCell = { cell with Column = col - 1 }
+                if (not (isCellEmpty cell) || not (isCellEmpty leftCell))
+                    && not (inSameDomino cell leftCell) then
+                    printf "|"
+                else
+                    printf " "
+                
                 match puzzle.Board[cell] with
-                    | Board.empty -> printf "  "
-                    | v -> printf $"{v} "
-            printfn ""
+                    | Board.empty -> printf "   "
+                    | v -> printf " %d " v
+            
+            let rightCell = Cell.create row (maxCol + 1)
+            let lastCell = Cell.create row maxCol
+            if (not (isCellEmpty lastCell) || not (isCellEmpty rightCell))
+                && not (inSameDomino lastCell rightCell) then
+                printfn "|"
+            else
+                printfn ""
+
+            // print bottom border for the last row
+        for col in 0 .. maxCol do
+            let cell = Cell.create maxRow col
+            let bottomCell = { cell with Row = maxRow + 1 }
+            if (not (isCellEmpty cell) || not (isCellEmpty bottomCell))
+                && not (inSameDomino cell bottomCell) then
+                printf "+---"
+            else
+                printf "+   "
+        printfn "+"
 
     let solveMany () =
 
@@ -80,4 +141,4 @@ module Program =
         for solution in solutions do
             printfn $"{printBoard solution}"
 
-    solveMany ()
+    solveOne ()
