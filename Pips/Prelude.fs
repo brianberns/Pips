2e36407f78d708c5355eb162292868bd1b525ecc
Brian Berns <brianberns@gmail.com>
2025-10-11 15:34:50 -0400

    Commented.

--- Pips/Prelude.fs
+++ Pips/Prelude.fs
@@ -3,6 +3,8 @@
 [<AutoOpen>]
 module Prelude =
 
+    /// Uncurries the given function.
     let uncurry f (a, b) = f a b
 
+    /// Flips the argument order for the given function.
     let flip f a b = f b a
