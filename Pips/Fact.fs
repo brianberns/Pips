c08f9d7d2dbacb0b26a6014f9a2ce31eeabdb5b3
Brian Berns <brianberns@gmail.com>
2025-10-17 13:56:21 -0400

    More work on fact-based approach.

--- Pips/Fact.fs
+++ Pips/Fact.fs
@@ -25,11 +25,9 @@ type CellFact =
     override fact.ToString () =
         $"{fact.Cell} {fact.Operator} {fact.Target}"
 
-type EdgeFact = CellFact * CellFact
+module CellFact =
 
-module Fact =
-
-    let private createCellFact cell region =
+    let create cell region =
         match region.Type with
             | RegionType.SumExact target ->
                 let op =
@@ -44,6 +42,23 @@ module Fact =
                 }
             | _ -> failwith "Unexpected"
 
+    let getStringency fact =
+        min
+            (fact.Target - PipCount.minValue)
+            (PipCount.maxValue - fact.Target)
+
+    let apply pipValue fact =
+        match fact.Operator with
+            | LessThan -> pipValue < fact.Target
+            | LessThanOrEqualTo -> pipValue <= fact.Target
+            | EqualTo -> pipValue = fact.Target
+            | GreaterThan -> pipValue > fact.Target
+            | GreaterThanOrEqualTo -> pipValue >= fact.Target
+
+type EdgeFact = CellFact * CellFact
+
+module EdgeFact =
+
     let createEdgeFacts (tiling : Tiling) puzzle =
         let regionMap =
             Map [
@@ -51,10 +66,35 @@ module Fact =
                     for cell in region.Cells do
                         cell, region
             ]
-        seq {
+        [|
             for (cellA, cellB) in tiling do
                 let regionA = regionMap[cellA]
                 let regionB = regionMap[cellB]
-                (createCellFact cellA regionA,
-                 createCellFact cellB regionB) : EdgeFact
-        }
+                (CellFact.create cellA regionA,
+                 CellFact.create cellB regionB) : EdgeFact
+        |]
+
+    let getStringency ((factA, factB) : EdgeFact) =
+        CellFact.getStringency factA + CellFact.getStringency factB
+
+    let tryApply domino ((factA, factB) : EdgeFact) =
+        if CellFact.apply domino.Left factA
+            && CellFact.apply domino.Right factB then
+            Some (factA.Cell, factB.Cell)
+        elif CellFact.apply domino.Left factB
+            && CellFact.apply domino.Right factA then
+            Some (factB.Cell, factA.Cell)
+        else None
+
+    let private applyEdgeFact edgeFact (dominoes : Set<Domino>) =
+        
+
+    let rec apply (edgeFacts : seq<EdgeFact>) (dominoes : Set<Domino>) =
+        let edgeFacts =
+            edgeFacts
+                |> Seq.sortBy getStringency
+                |> Seq.toList
+        match edgeFacts with
+            | [] -> []
+            | edgeFact :: rest ->
+                
