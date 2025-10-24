83924c3cdfead998a8c9be0b2e88a8af9feacae3
Brian Berns <brianberns@gmail.com>
2025-10-24 10:17:29 -0400

    Split safe array into two implementations.

--- Pips/Array2DSafe.fs
+++ Pips/Array2DSafe.fs
@@ -1,33 +1,5 @@
 ﻿namespace Pips
 
-#if FABLE_COMPILER
-
-type Array2DSafe<'t> = 't[(*row*)][(*column*)]
-
-module Array2DSafe =
-
-    let create rows cols value : Array2DSafe<_> =
-        Array.create rows (
-            Array.create cols value)
-
-    let copy (array : Array2DSafe<'t>) : Array2DSafe<'t> =
-        Array.init array.Length (fun r ->
-            Array.copy array[r])
-
-    let inline length0 (array : Array2DSafe<_>) =
-        array.Length
-
-    let inline length1 (array : Array2DSafe<_>) =
-        array[0].Length
-
-    let getItem row col (array : Array2DSafe<'t>) =
-        array[row][col]
-
-    let setItem row col value (array : Array2DSafe<'t>) =
-        array[row][col] <- value
-
-#else
-
 type Array2DSafe<'t> = 't[(*row*), (*column*)]
 
 module Array2DSafe =
@@ -49,5 +21,3 @@ module Array2DSafe =
 
     let inline setItem row col value (array : Array2DSafe<'t>) =
         array[row, col] <- value
-
-#endif
