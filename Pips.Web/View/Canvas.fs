e6276b1829c091999c9ef7b890461b0052a030e1
Brian Berns <brianberns@gmail.com>
2025-10-25 23:48:17 -0400

    Handle date pick event.

--- Pips.Web/View/Canvas.fs
+++ Pips.Web/View/Canvas.fs
@@ -1,5 +1,7 @@
 ﻿namespace Pips
 
+open System
+
 open Browser
 open Browser.Types
 
@@ -20,17 +22,24 @@ module Canvas =
     let private dailyUrl =
         "https://pips-dsa2dqawe8hrahf7.eastus-01.azurewebsites.net/api/daily"
 
-    let run () =
-
+    let puzzleDateInput =
+        document.getElementById "puzzle-date"
+            :?> HTMLInputElement
+    puzzleDateInput.onchange <- (fun _ ->
         let work =
             promise {
-                let dateStr = System.DateTime.Today.ToString("yyyy-MM-dd")
+                let date =
+                    puzzleDateInput.value
+                        |> DateTime.Parse
+                let dateStr = date.ToString("yyyy-MM-dd")
                 let! (daily : Daily) =
                     Fetch.get($"{dailyUrl}?date={dateStr}")
                 let puzzleMap = Daily.convert daily
                 console.log(puzzleMap["easy"])
             }
-        Promise.start work
+        Promise.start work)
+
+    let run () =
 
         ctx.beginPath()
         ctx.rect(100, 200, 300, 400)
