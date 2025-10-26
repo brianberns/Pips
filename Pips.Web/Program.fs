22e7368eb0f6e20b6cf42aef9908a3859e2da993
Brian Berns <brianberns@gmail.com>
2025-10-26 18:04:16 -0400

    Work on drawing puzzles.

--- Pips.Web/Program.fs
+++ Pips.Web/Program.fs
@@ -1,5 +1,28 @@
 ﻿namespace Pips
 
+open System
+
+open Browser
+open Browser.Types
+
+open Thoth.Fetch
+
 module Program =
 
-    Canvas.run()
+    let private dailyUrl =
+        "https://pips-dsa2dqawe8hrahf7.eastus-01.azurewebsites.net/api/daily"
+
+    let puzzleDateInput =
+        document.getElementById "puzzle-date"
+            :?> HTMLInputElement
+    puzzleDateInput.onchange <- (fun _ ->
+        promise {
+            let date =
+                puzzleDateInput.value
+                    |> DateTime.Parse
+            let dateStr = date.ToString("yyyy-MM-dd")
+            let! (daily : Daily) =
+                Fetch.get($"{dailyUrl}?date={dateStr}")
+            let puzzleMap = Daily.convert daily
+            Canvas.drawPuzzle(puzzleMap["easy"])
+        } |> ignore)
--- Pips.Web/View/Canvas.fs
+++ Pips.Web/View/Canvas.fs
@@ -1,13 +1,9 @@
 ﻿namespace Pips
 
-open System
-
 open Browser
 open Browser.Types
 
 open Fable.Core.JsInterop
-
-open Thoth.Fetch
 
 module Canvas =
 
@@ -19,28 +15,7 @@ module Canvas =
         // initialize drawing context
     let ctx = canvas.getContext_2d()
 
-    let private dailyUrl =
-        "https://pips-dsa2dqawe8hrahf7.eastus-01.azurewebsites.net/api/daily"
-
-    let puzzleDateInput =
-        document.getElementById "puzzle-date"
-            :?> HTMLInputElement
-    puzzleDateInput.onchange <- (fun _ ->
-        let work =
-            promise {
-                let date =
-                    puzzleDateInput.value
-                        |> DateTime.Parse
-                let dateStr = date.ToString("yyyy-MM-dd")
-                let! (daily : Daily) =
-                    Fetch.get($"{dailyUrl}?date={dateStr}")
-                let puzzleMap = Daily.convert daily
-                console.log(puzzleMap["easy"])
-            }
-        Promise.start work)
-
-    let run () =
-
+    let drawPuzzle (puzzle : Puzzle) =
         ctx.beginPath()
         ctx.rect(100, 200, 300, 400)
         ctx.fillStyle <- !^"blue"
