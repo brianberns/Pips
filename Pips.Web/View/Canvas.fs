30f870ee789a266308f90582bc5b4abc09426787
Brian Berns <brianberns@gmail.com>
2025-10-26 23:53:07 -0400

    More work on puzzle drawing.

--- Pips.Web/View/Canvas.fs
+++ Pips.Web/View/Canvas.fs
@@ -8,12 +8,6 @@ open Fable.Core.JsInterop
 
 module Canvas =
 
-    type CanvasRenderingContext2D with
-
-        [<Emit("($0).roundRect($1, $2, $3, $4, $5)")>]
-        member this.roundRect(x: float, y: float, w: float, h: float, radii: float) : unit =
-            failwith "JS interop"
-
         // initialize canvas
     let canvas =
         document.getElementById "canvas"
@@ -24,32 +18,42 @@ module Canvas =
 
     let cellSize = 50.0
 
-    let margin = 1.0
+    let offset = 5.0
 
     let getColor region =
         match region.Type with
             | RegionType.Any -> "whitesmoke"
             | _ ->
                 let range = 360
-                let count = 8
+                let count = 10
                 let hue = (region.GetHashCode() % (range / count)) * count
                 $"hsl({hue} 100%% 60%%)"
 
     let drawRegion region =
+
         for cell in region.Cells do
+
+            ctx.beginPath()
+
             let x = float cell.Column * cellSize
             let y = float cell.Row * cellSize
-            ctx.beginPath()
-            ctx.roundRect(
-                x + margin,
-                y + margin,
-                cellSize - 2.0 * margin,
-                cellSize - 2.0 * margin,
-                cellSize / 10.0)
+            ctx.rect(
+                x, y,
+                cellSize, cellSize)
+
             ctx.fillStyle <- !^(getColor region)
             ctx.fill()
 
+            ctx.strokeStyle <- !^"black"
+            ctx.stroke()
+
     let drawPuzzle puzzle =
+
         ctx.clearRect(0, 0, canvas.width, canvas.height)
+        ctx.translate(offset, offset)
+        ctx.lineWidth <- 2.0
+
         for region in puzzle.Regions do
             drawRegion region
+
+        ctx.setTransform(1, 0, 0, 1, 0, 0)   // resetTransform
