3035dcc1fc096285539bcdd6ea5c0c3b19ca829f
Brian Berns <brianberns@gmail.com>
2025-10-29 09:10:23 -0400

    Work on drawing dominoes.

--- Pips.Web/Domino.fs
+++ Pips.Web/Domino.fs
@@ -1,5 +1,6 @@
 ﻿namespace Pips.Web
 
+open System
 open Fable.Core.JsInterop
 open Pips
 
@@ -33,7 +34,7 @@ module Domino =
 
     let private drawPip (ctx : Context) radius x y =
         ctx.beginPath()
-        ctx.arc(x, y, radius, 0, 2.0 * System.Math.PI)
+        ctx.arc(x, y, radius, 0, 2.0 * Math.PI)
         ctx.fillStyle <- !^"black"
         ctx.fill()
 
@@ -70,39 +71,55 @@ module Domino =
             | 6 -> pip6 ()
             | _ -> failwith "Unexpected"
 
-    let cellSize = 40.0
     let outerStyle = 2.0, "black"
     let innerStyle = 1.0, "gray"
 
-    /// Draws the given unplaced domino at the given position.
-    let private drawUnplacedDomino (ctx : Context) x y domino =
+    /// Draws the given domino horizontally at the origin.
+    /// Callers use rotation and translatation to modify the
+    /// domino's location.
+    let private drawDomino (ctx : Context) cellSize domino =
 
-        let scale = 2.0 / 3.0
-        let cellSize = cellSize * scale
-
+            // draw the domino rectangle
         ctx.beginPath()
         ctx.roundRect(
-            x, y,
+            0, 0,
             cellSize * 2.0, cellSize,
             cellSize / 8.0)
         ctx.fillStyle <- !^(getDominoColor domino)
         ctx.fill()
-        ctx.lineWidth <- fst outerStyle * scale
+        ctx.lineWidth <- fst outerStyle
         ctx.strokeStyle <- !^(snd outerStyle)
         ctx.stroke()
 
+            // draw the dividing line
         ctx.beginPath()
-        ctx.moveTo(x + cellSize, y)
-        ctx.lineTo(x + cellSize, y + cellSize)
-        ctx.lineWidth <- fst innerStyle * scale
+        ctx.moveTo(cellSize, 0)
+        ctx.lineTo(cellSize, cellSize)
+        ctx.lineWidth <- fst innerStyle
         ctx.strokeStyle <- !^(snd innerStyle)
         ctx.stroke()
 
-        let leftX = x + (cellSize * 0.5)
-        let rightX = x + (cellSize * 1.5)
-        let centerY = y + (cellSize * 0.5)
+            // draw the pips
+        let leftX = cellSize * 0.5
+        let rightX = cellSize * 1.5
+        let centerY = cellSize * 0.5
         drawPipCount ctx cellSize leftX centerY domino.Left
         drawPipCount ctx cellSize rightX centerY domino.Right
+
+    let cellSize = 40.0
+
+    /// Draws the given unplaced domino at the given position.
+    let private drawUnplacedDomino (ctx : Context) x y domino =
+
+        ctx.save()
+
+        ctx.translate(x, y)
+        let scale = 2.0 / 3.0
+        ctx.scale(scale, scale)
+
+        drawDomino ctx cellSize domino
+
+        ctx.restore()
 
     /// Draws the given unplaced dominoes starting at the given
     /// Y position.
@@ -119,49 +136,24 @@ module Domino =
                 let y = startY + (float row * cellSize)
                 drawUnplacedDomino ctx x y domino
 
-    let private drawSolutionPipCount ctx cell pipCount =
-        let x = (float cell.Column + 0.5) * cellSize
-        let y = (float cell.Row + 0.5) * cellSize
-        drawPipCount ctx cellSize x y pipCount
-
     /// Draws the given placed domino at the given edge.
     let drawSolutionDomino
         (ctx : Context) domino ((cellA, cellB) : Edge) =
 
-        ctx.beginPath()
+        let row, col, nTwists =
+            match cellB.Row - cellA.Row, cellB.Column - cellA.Column with
+                |  0,  1 -> cellA.Row,     cellA.Column,     0   // horizontal
+                |  1,  0 -> cellA.Row,     cellA.Column + 1, 1   // vertical
+                |  0, -1 -> cellA.Row + 1, cellA.Column + 1, 2   // horizontal flipped
+                | -1,  0 -> cellA.Row + 1, cellA.Column,     3   // vertical flipped
+                | _ -> failwith "Unexpected"
 
-        let x =
-            float (min cellA.Column cellB.Column) * cellSize
-        let y =
-            float (min cellA.Row cellB.Row) * cellSize
+        ctx.save()
+        ctx.translate(
+            float col * cellSize,
+            float row * cellSize)
+        ctx.rotate(float nTwists * Math.PI / 2.0)
 
-        let isHorizontal = cellA.Row = cellB.Row
-        let width =
-            if isHorizontal then cellSize * 2.0
-            else cellSize
-        let height =
-            if isHorizontal then cellSize
-            else cellSize * 2.0
-        ctx.roundRect(
-            x, y,
-            width, height,
-            cellSize / 8.0)
-        ctx.fillStyle <- !^(getDominoColor domino)
-        ctx.fill()
-        ctx.lineWidth <- fst outerStyle
-        ctx.strokeStyle <- !^(snd outerStyle)
-        ctx.stroke()
+        drawDomino ctx cellSize domino
 
-        ctx.beginPath()
-        if isHorizontal then
-            ctx.moveTo(x + cellSize, y)
-            ctx.lineTo(x + cellSize, y + cellSize)
-        else
-            ctx.moveTo(x, y + cellSize)
-            ctx.lineTo(x + cellSize, y + cellSize)
-        ctx.lineWidth <- fst innerStyle
-        ctx.strokeStyle <- !^(snd innerStyle)
-        ctx.stroke()
-
-        drawSolutionPipCount ctx cellA domino.Left
-        drawSolutionPipCount ctx cellB domino.Right
+        ctx.restore()
