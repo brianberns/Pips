f3aff10373eab3e3472702d360cf907189573ae9
Brian Berns <brianberns@gmail.com>
2026-07-28 06:42:47 -0400

    More work on proofs.

--- Pips/Proof.fs
+++ Pips/Proof.fs
@@ -2,19 +2,42 @@
 
 module Proof =
 
-    let rec solve (puzzle : Puzzle) =
+    let private toTilingMap tilings =
+        tilings
+            |> Seq.collect (fun (tiling : Tiling) ->
+                tiling
+                    |> Seq.map (fun edge ->
+                        let tiling = tiling.Remove(edge)
+                        edge, (tiling : Tiling)))
+            |> Seq.groupBy fst
+            |> Seq.map (fun (edge, group) ->
+                edge, Seq.map snd group)
+            |> Map
 
-        let edges = 
+    let solve puzzle =
+
+        let rec search tilingMap puzzle =
+            [|
+                for domino in puzzle.UnplacedDominoes do
+                    for (edge, tilings) in Map.toSeq tilingMap do
+                        yield! loop tilings domino edge puzzle
+                        if not (Domino.isDouble domino) then
+                            let edge = Edge.reverse edge
+                            yield! loop tilings domino edge puzzle
+            |]
+
+        and loop tilings domino edge puzzle =
+            [|
+                match Puzzle.tryPlace domino edge puzzle with
+                    | Some puzzle ->
+                        let tilingMap = toTilingMap tilings
+                        yield domino, edge
+                        yield! search tilingMap puzzle
+                    | None -> ()
+            |]
+
+        let tilingMap =
             Puzzle.getAllTilings puzzle
-                |> Seq.concat
-                |> Seq.distinct
+                |> toTilingMap
 
-        seq {
-            for domino in puzzle.UnplacedDominoes do
-                for edge in edges do
-                    match Puzzle.tryPlace domino edge puzzle with
-                        | Some puzzle ->
-                            yield domino, edge
-                            yield! solve puzzle
-                        | None -> ()
-        }
+        search tilingMap puzzle
