a73d9290decf44faa222438ba60f17e6e42fea2d
Brian Berns <brianberns@gmail.com>
2026-07-28 07:03:11 -0400

    More work on proofs.

--- Pips/Proof.fs
+++ Pips/Proof.fs
@@ -12,27 +12,34 @@ module Proof =
             |> Seq.groupBy fst
             |> Seq.map (fun (edge, group) ->
                 edge, Seq.map snd group)
+            |> Map
 
     let solve puzzle =
 
         let rec search tilingMap puzzle =
             [|
                 for domino in puzzle.UnplacedDominoes do
-                    for (edge, tilings) in tilingMap do
-                        yield! loop tilings domino edge puzzle
-                        if not (Domino.isDouble domino) then
-                            let edge = Edge.reverse edge
-                            yield! loop tilings domino edge puzzle
-            |]
 
-        and loop tilings domino edge puzzle =
-            [|
-                match Puzzle.tryPlace domino edge puzzle with
-                    | Some puzzle ->
-                        let tilingMap = toTilingMap tilings
+                    let pairs =
+                        [|
+                            for (edge, tilings) in Map.toSeq tilingMap do
+                                let canPlace edge =
+                                    Puzzle.tryPlace domino edge puzzle
+                                        |> Option.isSome
+                                if canPlace edge then edge, tilings
+                                if not (Domino.isDouble domino) then
+                                    let edge = Edge.reverse edge
+                                    if canPlace edge then edge, tilings
+                        |]
+
+                    if pairs.Length = 1 then
+
+                        let edge, tilings = Array.exactlyOne pairs
                         yield domino, edge
+
+                        let puzzle = Puzzle.place domino edge puzzle
+                        let tilingMap = toTilingMap tilings
                         yield! search tilingMap puzzle
-                    | None -> ()
             |]
 
         let tilingMap =
