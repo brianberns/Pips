3dbdfda9f723705cb6828e3a4e70489a5be3d8dc
Brian Berns <brianberns@gmail.com>
2026-07-27 23:53:56 -0400

    Commented.

--- Pips/Array2DSafe.fs
+++ Pips/Array2DSafe.fs
@@ -1,60 +1,63 @@
 ﻿namespace Pips
 
-(*
- * A 2D array of values. JavaScript has no such thing, so under
- * Fable this is a jagged array instead. Both are used through
- * the same functions, so callers don't have to care which one
- * they have.
- *)
-
+/// A 2D array of values. JavaScript doesn't support
+/// this directly, so we provide Fable with a jagged
+/// array instead.
 #if FABLE_COMPILER
-
 type Array2DSafe<'t> = 't[(*row*)][(*column*)]
+#else
+type Array2DSafe<'t> = 't[(*row*), (*column*)]
+#endif
 
 module Array2DSafe =
 
+    /// Creates a 2D array whose elements all have the
+    /// given value.
     let create rows cols value : Array2DSafe<_> =
+#if FABLE_COMPILER
         Array.init rows (fun _ ->            // a separate array for each row
             Array.create cols value)
+#else
+        Array2D.create rows cols value
+#endif
 
+    /// Copies the given 2D array.
     let inline copy (array : Array2DSafe<'t>) : Array2DSafe<'t> =
+#if FABLE_COMPILER
         Array.init array.Length (fun row ->
             Array.copy array[row])
-
-    let inline length0 (array : Array2DSafe<_>) =
-        array.Length
-
-    let inline length1 (array : Array2DSafe<_>) =
-        array[0].Length
-
-    let inline getItem row col (array : Array2DSafe<'t>) =
-        array[row][col]
-
-    let inline setItem row col value (array : Array2DSafe<'t>) =
-        array[row][col] <- value
-
 #else
-
-type Array2DSafe<'t> = 't[(*row*), (*column*)]
-
-module Array2DSafe =
-
-    let create rows cols value : Array2DSafe<_> =
-        Array2D.create rows cols value
-
-    let inline copy (array : Array2DSafe<_>) : Array2DSafe<_> =
         Array2D.copy array
+#endif
 
+    /// Length of dimension 0.
     let inline length0 (array : Array2DSafe<_>) =
+#if FABLE_COMPILER
+        array.Length
+#else
         array.GetLength(0)
+#endif
 
+    /// Length of dimension 1.
     let inline length1 (array : Array2DSafe<_>) =
+#if FABLE_COMPILER
+        array[0].Length
+#else
         array.GetLength(1)
+#endif
 
+    /// Gets the element with the given indexes.
     let inline getItem row col (array : Array2DSafe<'t>) =
+#if FABLE_COMPILER
+        array[row][col]
+#else
         array[row, col]
+#endif
 
+    /// Sets the element with the given indexes.
     let inline setItem row col value (array : Array2DSafe<'t>) =
+#if FABLE_COMPILER
+        array[row][col] <- value
+#else
         array[row, col] <- value
-
 #endif
