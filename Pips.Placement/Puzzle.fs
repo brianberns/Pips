eccfca1095ce655eb7b69e8064dd55bb63ef4cb5
Brian Berns <brianberns@gmail.com>
2026-08-02 16:29:15 -0400

    Commented.

--- Pips.Placement/Puzzle.fs
+++ Pips.Placement/Puzzle.fs
@@ -2,13 +2,19 @@ namespace Pips
 
 module Puzzle =
 
+    /// Is the given puzzle in a valid state for the
+    /// given tilings?
     let isValidTiled (tilings : seq<Tiling>) puzzle =
+
+            // gather cells that are in `Equal` regions
         let cellSets =
             puzzle.Regions
                 |> Seq.where (fun region ->
                     region.Type = RegionType.Equal)
                 |> Seq.map (_.Cells >> set)
                 |> Seq.toArray
+
+            // look for required edges that are wholy contained in an `Equal` region
         let nRequired =
             Set.intersectMany tilings
                 |> Seq.choose (fun (cellA, cellB) ->
@@ -17,9 +23,11 @@ module Puzzle =
                             cells.Contains(cellA)
                                 && cells.Contains(cellB)))
                 |> Seq.length
+
+            // such edges require doubles
         let nAvailable =
             puzzle.UnplacedDominoes
                 |> Seq.where Domino.isDouble
                 |> Seq.length
-        nAvailable >= nRequired
 
+        nAvailable >= nRequired
